4c1079bc28a8b3e1a478aa4c4325714198fd60b6
Franz Almeria <f.ranza.lmeria.baymax12@gmail.com>
2022-05-08 03:10:42 +0800

    created sign up component, the form input component, button component

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app'
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth'
 import {
   getFirestore,
@@ -23,22 +23,26 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
   prompt: 'select_account'
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+
+  if (!userAuth) return
+
   const userDocRef = doc(db, 'users', userAuth.uid)
-  console.log(userDocRef)
   const userSnapShot = await getDoc(userDocRef)
-  console.log(userSnapShot)
-  console.log(userSnapShot.exists())
 
   if (!userSnapShot.exists()) {
     const { displayName, email } = userAuth
@@ -48,10 +52,16 @@ export const createUserDocumentFromAuth = async (userAuth) => {
       await setDoc(userDocRef, {
         displayName,
         email,
-        createdAt
+        createdAt,
+        ...additionalInformation,
       })
     } catch (error) {
       console.log('error catching the user', error.message)
     }
   }
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
