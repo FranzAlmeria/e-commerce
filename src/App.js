04d671c5dba1ad946ab0041fe0ab6f9195957ce8
Franz Almeria <f.ranza.lmeria.baymax12@gmail.com>
2022-05-09 14:49:51 +0800

    created authentication component page alongside with sign in form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.routes';
 import Navigation from './routes/navigation/navigation.routes';
-import SignIn from './routes/sign-in/sign-in.routes';
+import Authentication from './routes/authentication/authentication.routes';
 
 import './App.css';
 
@@ -19,7 +19,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/authentication/authentication.routes.jsx
@@ -0,0 +1,16 @@
+import SignUpForm from '../../components/sign-up-form/sign-up-form.components.jsx';
+import SignInForm from '../../components/sign-in-form/sign-in-form.components.jsx';
+import './authentication.styles.scss'
+
+
+const Authentication = () => {
+
+  return (
+    <div className='authentication-container'>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default Authentication
